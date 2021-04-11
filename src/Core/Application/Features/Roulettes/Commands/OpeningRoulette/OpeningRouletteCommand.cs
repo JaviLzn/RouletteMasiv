f66fd4c548117cda0b9bbde7256b0e8f2c2e23f4
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 18:39:03 -0500

    Create handler command for opening roulette

--- src/Core/Application/Features/Roulettes/Commands/OpeningRoulette/OpeningRouletteCommand.cs
+++ src/Core/Application/Features/Roulettes/Commands/OpeningRoulette/OpeningRouletteCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Application.Features.Roulettes.Commands.OpeningRoulette
 {
-    public class OpeningRouletteCommand : IRequest<string>
+    public class OpeningRouletteCommand : IRequest<OpeningRouletteResponse>
     {
         public string Id { get; set; }
     }
---
+++ src/Core/Application/Features/Roulettes/Commands/OpeningRoulette/OpeningRouletteResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Roulettes.Commands.OpeningRoulette
+{
+    public class OpeningRouletteResponse
+    {
+        public string RouletteId { get; set; }
+        public string RouletteCurrentStatus { get; set; }
+        public string OperationStatus { get; set; }
+    }
+}
