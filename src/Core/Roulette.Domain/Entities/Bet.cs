48ec748cef43b472def0192904c0e384b930040d
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 13:06:00 -0500

    Create class for bets

---
+++ src/Core/Roulette.Domain/Entities/Bet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Roulette.Domain.Entities
+{
+    public class Bet
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Color { get; set; }
+        public int Amount { get; set; }
+        public string UserId { get; set; }
+    }
+}
