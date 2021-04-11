2a9677717b5c93c00b51b527795f6456fdcb8125
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 18:38:17 -0500

    Use roulette status on create roulette. Clean unused "using statements"

--- src/Core/Application/Features/Roulettes/Commands/CreateRoulette/CreateRouletteResponse.cs
+++ src/Core/Application/Features/Roulettes/Commands/CreateRoulette/CreateRouletteResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Application.Features.Roulettes.Commands.CreateRoulette
+﻿namespace Application.Features.Roulettes.Commands.CreateRoulette
 {
     public class CreateRouletteResponse
     {
