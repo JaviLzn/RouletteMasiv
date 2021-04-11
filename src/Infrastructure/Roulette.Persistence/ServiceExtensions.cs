8a3165be210d6b75a3f17b341689ddbc96813d1c
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 13:46:41 -0500

    Create service extension for persistence layer

---
+++ src/Infrastructure/Roulette.Persistence/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Roulette.Domain.Settings;
+using StackExchange.Redis;
+
+namespace Roulette.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton(sp =>
+            {
+                var settings = sp.GetRequiredService<IOptions<ConnectionSettings>>().Value;
+                var configuration = ConfigurationOptions.Parse(settings.ConnectionString, true);
+                configuration.ResolveDns = true;
+
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+        }
+    }
+}
