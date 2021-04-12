9c0180227de840fdac328fdc07b6c7c8571ba632
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 20:28:24 -0500

    Create class for request and responde of bet action

---
+++ src/Core/Application/Features/Roulettes/Commands/BetRoulette/BetRouletteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Roulettes.Commands.BetRoulette
+{
+    public class BetRouletteRequest
+    {
+        public int? Number { get; set; }
+        public string Color { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ src/Core/Application/Features/Roulettes/Commands/BetRoulette/BetRouletteResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Features.Roulettes.Commands.BetRoulette
+{
+    public class BetRouletteResponse
+    {
+        public string BetStatus { get; set; }
+        public List<string> ValidationFailures { get; set; }
+    }
+}
