467ef8b4ab3f39c1fb16f91b39cf536edadb03d3
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 22:19:34 -0500

    Create Ending Roulette command Handle this command Validate this command Create a response for command

---
+++ src/Core/Application/Features/Roulettes/Commands/EndingRoulette/EndingRouletteResponse.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace Application.Features.Roulettes.Commands.EndingRoulette
+{
+    public class EndingRouletteResponse
+    {
+        public string RouletteId { get; set; }
+        public string RouletteCurrentStatus { get; set; }
+        public string OperationStatus { get; set; }
+        public List<Bet> Bets { get; set; }
+        public int WinnerNumber { get; set; }
+        public List<string> ValidationFailures { get; set; }
+    }
+}
---
+++ src/Core/Application/Features/Roulettes/Commands/EndingRoulette/EndingRouletteValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Helpers;
+using Application.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.Roulettes.Commands.EndingRoulette
+{
+    public class EndingRouletteValidator : AbstractValidator<EndingRouletteCommand>
+    {
+        private readonly IRouletteRepository rouletteRepository;
+        public EndingRouletteValidator(IRouletteRepository rouletteRepository)
+        {
+            this.rouletteRepository = rouletteRepository;
+            Rules();
+        }
+
+        private void Rules()
+        {
+            RuleFor(expression: p => p.RouletteId).NotEmpty().WithMessage(errorMessage: "Roulette not specified");
+            RuleFor(expression: p => p.RouletteId).MustAsync(async (RouletteId, cancellationToken) => await ValidatorHelper.RouletteExistAsync(rouletteId: RouletteId, rouletteRepository: rouletteRepository)).WithMessage(errorMessage: "Roulette not found.");
+            RuleFor(expression: p => p.RouletteId).MustAsync(async (RouletteId, cancellationToken) => await ValidatorHelper.RouletteIsOpen(rouletteId: RouletteId, rouletteRepository: rouletteRepository)).WithMessage(errorMessage: "Roulette must to be open");
+        }
+    }
+}
