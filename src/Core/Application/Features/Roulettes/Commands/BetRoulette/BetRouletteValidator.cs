68e8b1096d7993c3015ca6eecfbea5e07a0e8863
Javier Lozano <ing.javierlozano@hotmail.com>
2021-04-11 21:36:11 -0500

    Create a helper for validators

--- src/Core/Application/Features/Roulettes/Commands/BetRoulette/BetRouletteValidator.cs
+++ src/Core/Application/Features/Roulettes/Commands/BetRoulette/BetRouletteValidator.cs
@@ -1,8 +1,7 @@
 ﻿using Application.Enum;
+using Application.Helpers;
 using Application.Interfaces;
 using FluentValidation;
-using System;
-using System.Threading.Tasks;
 
 namespace Application.Features.Roulettes.Commands.BetRoulette
 {
@@ -18,8 +17,8 @@ namespace Application.Features.Roulettes.Commands.BetRoulette
         private void Rules()
         {
             RuleFor(expression: p => p.RouletteId).NotEmpty().WithMessage(errorMessage: "Roulette not specified");
-            RuleFor(expression: p => p.RouletteId).MustAsync(async (RouletteId, cancellationToken) => await RouletteExistAsync(RouletteId: RouletteId)).WithMessage(errorMessage: "Roulette not found.");
-            RuleFor(expression: p => p.RouletteId).MustAsync(async (RouletteId, cancellationToken) => await RouletteIsOpen(RouletteId: RouletteId)).WithMessage(errorMessage: "Roulette must to be open");
+            RuleFor(expression: p => p.RouletteId).MustAsync(async (RouletteId, cancellationToken) => await ValidationHelper.RouletteExistAsync(RouletteId: RouletteId, rouletteRepository: rouletteRepository)).WithMessage(errorMessage: "Roulette not found.");
+            RuleFor(expression: p => p.RouletteId).MustAsync(async (RouletteId, cancellationToken) => await ValidationHelper.RouletteIsOpen(RouletteId: RouletteId, rouletteRepository: rouletteRepository)).WithMessage(errorMessage: "Roulette must to be open");
             RuleFor(expression: p => p.Color).Empty().When(p => p.Number != null).WithMessage(errorMessage: "Color should not be sent when it is sent Number.");
             RuleFor(expression: p => p.Color).IsEnumName(enumType: typeof(RouletteColors)).When(predicate: p => p.Color != null).WithMessage(errorMessage: "Color not valid");
             RuleFor(expression: p => p.Number).Empty().When(p => !string.IsNullOrEmpty(p.Color)).WithMessage(errorMessage: "Number should not be sent when it is sent Color."); ;
@@ -29,18 +28,6 @@ namespace Application.Features.Roulettes.Commands.BetRoulette
             RuleFor(expression: p => p.Amount).LessThanOrEqualTo(valueToCompare: 10000).WithMessage(errorMessage: "Amount must to be less than or equal to 10.000 USD");
             RuleFor(expression: p => p.UserId).NotEmpty().WithMessage(errorMessage: "UserId not specified");
             RuleFor(expression: p => p).Must(predicate: p => !(p.Number == null && p.Color == null)).WithMessage("You must bet on Color or Number");
-        }
-
-        private async Task<bool> RouletteExistAsync(string RouletteId)
-        {
-            Guid.TryParse(RouletteId, out Guid roulleteId);
-            return await rouletteRepository.GetByIdAsync(rouletteId: roulleteId) != null;
-        }
-
-        private async Task<bool> RouletteIsOpen(string RouletteId)
-        {
-            Guid.TryParse(RouletteId, out Guid roulleteId);
-            return (await rouletteRepository.GetByIdAsync(rouletteId: roulleteId))?.Status == RouletteStatus.Open.ToString();
         }
     }
 }
